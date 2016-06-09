0fe85653e75fa9e1c5eb0dd39a200ac6ed2158cd
JamesWinestock <james.winestock@gmail.com>
2016-06-09 11:52:44 +1000

    Quotes cycle through on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
+  include HomeHelper
+
   def index
     @users = User.all.order(created_at: :desc)
     @user = current_user
+    @daily_quote = INSPIRATION.sample
   end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,6 @@
 module HomeHelper
+
+  INSPIRATION = ['Learning is not attained by chance, it must be sought for with ardor and attended to with diligence', 'Learning is a treasure that will follow its owner everywhere', 'He who is afraid of asking is ashamed of learning']
+
+  
 end
