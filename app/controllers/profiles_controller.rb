24c504a734e47358f5459fc6abc35c3b232b62df
JamesWinestock <james.winestock@gmail.com>
2016-06-09 10:16:43 +1000

    Profiles working

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,41 @@
+class ProfilesController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @profile = @user.profile
+
+  end
+
+  def new
+    @user = current_user
+    @profile = @user.profile
+  end
+
+  def edit
+    @user = current_user
+    @profile = @user.profile
+  end
+
+  def update
+    @user = current_user
+    if @profile = current_user.profile
+      @profile.update(profile_params)
+      flash[:notice] = "Success"
+      redirect_to profile_path(@user)
+    else
+      flash.now[:notice] = "Something went wrong :("
+      render :edit
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:bio, :user_id)
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:new, :create], raise: false
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = current_user
+    @profile = @user.profile
+  end
+
+  def create
+    @user = sign_up(user_params)
+
+    if @user.valid?
+      sign_in(@user)
+      @profile = Profile.new
+      @profile.user_id = @user.id
+      @profile.save
+      redirect_to home_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
+  end
+end
